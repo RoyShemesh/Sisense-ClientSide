54b2ca34ff805e8fbda2435fd96749192d331019
Roy Shemesh <89579074+RoyShemesh@users.noreply.github.com>
2022-04-13 16:38:25 +0300

    Build clientcasing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from './core/hompePage/HomePage';
+import NavationBar from './core/navBar/NavigationBar';
+import './styles/main.css';
+
+function App() {
+	return (
+		<div className="">
+			<NavationBar />
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
