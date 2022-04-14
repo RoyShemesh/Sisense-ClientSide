95234ae84fa1f19189791aba5d1dfff9a86d6d5d
Roy Shemesh <89579074+RoyShemesh@users.noreply.github.com>
2022-04-14 20:35:47 +0300

    File arrangement
    -Arrange search form to be more neatly

--- src/core/hompePage/searchForm/SearchForm.tsx
+++ src/core/hompePage/searchForm/SearchForm.tsx
@@ -12,6 +12,18 @@ export default function SearchForm({ setStatus, setPerTimeStamp, setFrom, setTo 
 	useEffect(() => {
 		fetchDataByDatesRange('2021-06-01', '2021-12-31', setStatus);
 	}, []);
+	const searchDates = async () => {
+		const from = fromInput.current?.value;
+		const to = toInput.current?.value;
+		if (from && to) {
+			await fetchDataByDatesRange(from, to, setStatus);
+			setFrom(from);
+			setTo(to);
+		} else {
+			const notyf = new Notyf();
+			notyf.error('Please enter full date');
+		}
+	};
 	return (
 		<div className="md:flex hidden flex-col items-center ">
 			<div className="flex items-center ">
@@ -22,18 +34,7 @@ export default function SearchForm({ setStatus, setPerTimeStamp, setFrom, setTo 
 				<button
 					className="m-1 mx-2 text-lg font-thin transition md:w-full ease-in-out duration-150 shadow bg-sky-600 md:transform hover:scale-110  focus:outline-none text-white  py-1 px-4 rounded"
 					type="button"
-					onClick={async () => {
-						const from = fromInput.current?.value;
-						const to = toInput.current?.value;
-						if (from && to) {
-							await fetchDataByDatesRange(from, to, setStatus);
-							setFrom(from);
-							setTo(to);
-						} else {
-							const notyf = new Notyf();
-							notyf.error('Please enter full date');
-						}
-					}}
+					onClick={() => searchDates()}
 				>
 					Filter dates by:
 				</button>
